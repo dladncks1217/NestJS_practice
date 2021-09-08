dd7f2930b75474742e5e8da65942887c0fe177e3
dladncks1217 <dlaxodud1217@gmail.com>
2021-09-08 20:20:55 +0900

    Add: custom Decorator(User), Dependencies

--- typeormpractice/src/users/users.controller.ts
+++ typeormpractice/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorator/user.decorator';
+import { UsersService } from './users.service';
 
+
+@ApiTags('USER')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private UserService:UsersService){}
+
+    @Get()
+    getUsers(@User() user){
+        return user;
+    }
+
+}
--- typeormpractice/src/users/users.service.ts
+++ typeormpractice/src/users/users.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    postUser(email:string, name:string, age:number){
+
+    }
+}
