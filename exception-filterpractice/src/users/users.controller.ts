121d3a89f42b16cde94aced6997326b4c6c691fc
dladncks1217 <dlaxodud1217@gmail.com>
2021-09-15 20:54:42 +0900

    Add: add UndefinedToNullInterceptor

--- exception-filterpractice/src/users/users.controller.ts
+++ exception-filterpractice/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { undefinedToNullInterceptor } from 'src/interceptors/undefinedToNull.interceptor';
+import { UsersService } from './users.service';
 
+@UseInterceptors(undefinedToNullInterceptor)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private UsersService:UsersService){}
+
+    @Post()
+    async Join(@Body() body){
+        await this.UsersService.Join(body.email, body.name, body.password);
+    }
+}
--- exception-filterpractice/src/users/users.service.ts
+++ exception-filterpractice/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/User';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)  // 이거 인젝션하는법 module파일에서 TypeOrmModule.forFeature쓰자.
+        private usersRepository: Repository<User>,
+    ){}
+    
+    async Join(email:string, name:string, password:string){
+
+    }
+    
+}
