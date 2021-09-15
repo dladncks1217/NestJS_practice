bab20dc4aadb7e329c510c880d4d8ebaed2acaa2
dladncks1217 <dlaxodud1217@gmail.com>
2021-09-15 17:22:15 +0900

    Update: API Document Name Modify

--- typeormpractice-2/src/main.ts
+++ typeormpractice-2/src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   const config = new DocumentBuilder()
-  .setTitle('Sleact API')
-  .setDescription('Sleact 개발을 위한 API 문서입니다.')
+  .setTitle('API')
+  .setDescription('API 문서입니다.')
   .setVersion('1.0')
   .addCookieAuth('connect.sid')
   .build();
