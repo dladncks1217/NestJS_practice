f7683982487c28e87e8afab62116f4e51dbaf1fd
dladncks1217 <dlaxodud1217@gmail.com>
2021-09-20 13:53:08 +0900

    Update: typo correction(LocalStrategy)

--- passportpractice/src/auth/local.strategy.ts
+++ passportpractice/src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException, Logger } from "@nestjs/common";
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
@@ -7,9 +7,10 @@ import { AuthService } from "./auth.service";
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy){
     constructor(private authService:AuthService){
-        super({usernameFeild:'email', passwordFeild:'password'});
+        super({usernameField:'email', passwordField:'password'});
     }
     async validate(email:string, password:string, done:CallableFunction){
+        
         const user = await this.authService.validateUser(email, password);
         if(!user){
             throw new UnauthorizedException(); // 401던져주는 친구. 여기에 걸리면 httpExceptionFilter.ts에 걸림.
--- passportpractice/src/main.ts
+++ passportpractice/src/main.ts
@@ -12,6 +12,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
   app.useGlobalPipes(new ValidationPipe()); // httpException.filter.ts 통해 에러메시지 변경 가능
   app.useGlobalFilters(new HttpExceptionFilter); // 모든 컨트롤러에서 발생하는 HttpException을 얘가 걸러줄거임.
 
