2e346a69c432e774b0f2a471d52fa5a365349f4c
dladncks1217 <dlaxodud1217@gmail.com>
2021-10-05 15:06:59 +0900

    init

--- lifecycle-example/src/app.module.ts
+++ lifecycle-example/src/app.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { User } from './entities/User';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import *as ormconfig from '../ormconfig';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    })
+    UsersModule,
+    User,
+    AuthModule,
+    TypeOrmModule.forRoot(ormconfig),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [
+    AppController, 
+    UsersController
+  ],
+  providers: [
+    AppService, 
+    UsersService
+  ],
 })
 export class AppModule {}
