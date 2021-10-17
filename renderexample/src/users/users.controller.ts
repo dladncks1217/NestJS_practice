a00010b94ba88f8a6390b5c6a8514bfcdce797ed
dladncks1217 <dlaxodud1217@gmail.com>
2021-10-17 15:01:01 +0900

    Add: add DTO, decorator, controller, service

--- renderexample/src/users/users.controller.ts
+++ renderexample/src/users/users.controller.ts
@@ -1,6 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Users } from 'src/common/decorator/user.decorator';
+import { UsersService } from './users.service';
 
 @ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private UsersService: UsersService) {}
+
+  @ApiOperation({ summary: '유저 정보 조회' })
+  @Get('getuser')
+  getUserInformation(@Users() user) {
+    return user;
+  }
+
+  @ApiOperation({ summary: '회원가입' })
+  @Post('join')
+  async Join(@Body() body) {
+    await this.UsersService.Join(
+      body.email,
+      body.name,
+      body.age,
+      body.password,
+    );
+  }
+
+  @ApiOperation({ summary: '로그인' })
+  @Post('login')
+  LogIn() {}
+
+  @ApiOperation({ summary: '로그아웃' })
+  @Post('logout')
+  LogOut() {}
+}
