87ea9e9127e3a1ffa65e5a1c76ccffa812ba9557
dladncks1217 <dlaxodud1217@gmail.com>
2021-12-17 01:13:23 +0900

    init

--- axioscheck/nestback/src/app.service.ts
+++ axioscheck/nestback/src/app.service.ts
@@ -1,15 +1,19 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosResponse } from 'axios';
+import axios, { Axios, AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
   getHello(): string {
     return 'Hello World!';
   }
-  async getAxiosReturn() {
-    const result = await axios.get('http://localhost:8000');
+
+  async getAxiosReturn(): Promise<any> {
+    const result = await this.httpService
+      .get('http://localhost:8000')
+      .toPromise();
     return result.data;
   }
 }
