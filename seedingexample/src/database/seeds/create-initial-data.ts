a2decf78cb3e29009506576ded393be955813efa
dladncks1217 <dlaxodud1217@gmail.com>
2022-02-18 17:01:52 +0900

    faker seeding

--- seedingexample/src/database/seeds/create-initial-data.ts
+++ seedingexample/src/database/seeds/create-initial-data.ts
@@ -1,20 +1,23 @@
+import faker from '@faker-js/faker';
 import { Connection } from 'typeorm';
 import { Seeder, Factory } from 'typeorm-seeding';
 import { Users } from '../../entities/Users';
 
 export class CreateInitialData implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
+    const data = [];
+    for (let i = 14; i < 27; i++) {
+      data.push({
+        id: i,
+        name: faker.name.findName(),
+        age: Math.floor(Math.random() * 50),
+      });
+    }
     await connection
       .createQueryBuilder()
       .insert()
       .into(Users)
-      .values([
-        {
-          id: 1,
-          name: '임우찬',
-          age: 24,
-        },
-      ])
+      .values(data)
       .execute();
   }
 }
